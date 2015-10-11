0236f92fe3d6e0fc0b32d9c3d26281cf0315f2c5
laurileemet <laurii12345@gmail.com>
2015-10-11 15:41:20 +0300

    Added project settings
    To avoid all the confing

---
+++ Debug/main.pp
@@ -0,0 +1,49 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: K:/UserFolders/My\ Documents/GitHub/SardNFC/NFC/NFC.h
+main.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430.h
+main.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h
+main.obj: C:/ti/ccsv6/ccs_base/msp430/include/in430.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics_legacy_undefs.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/stdlib.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/linkage.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/stdio.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/stdarg.h
+main.obj: K:/UserFolders/My\ Documents/GitHub/SardNFC/NFC/iso14443a.h
+main.obj: K:/UserFolders/My\ Documents/GitHub/SardNFC/Hardware/mcu.h
+main.obj: K:/UserFolders/My\ Documents/GitHub/SardNFC/Hardware/Trf7970BoosterPack.h
+main.obj: K:/UserFolders/My\ Documents/GitHub/SardNFC/Hardware/types.h
+main.obj: K:/UserFolders/My\ Documents/GitHub/SardNFC/Hardware/mcu.h
+main.obj: K:/UserFolders/My\ Documents/GitHub/SardNFC/Hardware/trf7970.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/stdlib.h
+main.obj: K:/UserFolders/My\ Documents/GitHub/SardNFC/NFC/iso14443a.h
+main.obj: K:/UserFolders/My\ Documents/GitHub/SardNFC/Hardware/spi.h
+main.obj: K:/UserFolders/My\ Documents/GitHub/SardNFC/Hardware/uart.h
+main.obj: K:/UserFolders/My\ Documents/GitHub/SardNFC/NFC/iso14443b.h
+main.obj: K:/UserFolders/My\ Documents/GitHub/SardNFC/NFC/iso15693.h
+
+../main.c: 
+K:/UserFolders/My\ Documents/GitHub/SardNFC/NFC/NFC.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/intrinsics_legacy_undefs.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/stdlib.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/linkage.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/stdio.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/stdarg.h: 
+K:/UserFolders/My\ Documents/GitHub/SardNFC/NFC/iso14443a.h: 
+K:/UserFolders/My\ Documents/GitHub/SardNFC/Hardware/mcu.h: 
+K:/UserFolders/My\ Documents/GitHub/SardNFC/Hardware/Trf7970BoosterPack.h: 
+K:/UserFolders/My\ Documents/GitHub/SardNFC/Hardware/types.h: 
+K:/UserFolders/My\ Documents/GitHub/SardNFC/Hardware/mcu.h: 
+K:/UserFolders/My\ Documents/GitHub/SardNFC/Hardware/trf7970.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_4.4.5/include/stdlib.h: 
+K:/UserFolders/My\ Documents/GitHub/SardNFC/NFC/iso14443a.h: 
+K:/UserFolders/My\ Documents/GitHub/SardNFC/Hardware/spi.h: 
+K:/UserFolders/My\ Documents/GitHub/SardNFC/Hardware/uart.h: 
+K:/UserFolders/My\ Documents/GitHub/SardNFC/NFC/iso14443b.h: 
+K:/UserFolders/My\ Documents/GitHub/SardNFC/NFC/iso15693.h: 
